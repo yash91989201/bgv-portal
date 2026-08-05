bd543dc507e623cfc8656d566e4c4aa076d1af69
Yashraj Jaiswal <yashraj6370@gmail.com>
2026-08-05 15:35:34 +0530

    update title

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -35,7 +35,7 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
 				content: "width=device-width, initial-scale=1",
 			},
 			{
-				title: "My App",
+				title: "BGV Portal",
 			},
 		],
 		links: [
